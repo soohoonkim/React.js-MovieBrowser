cc506b490deb913f835a3746256afbd02f41944b
soohoonkim <soohoon.kim@hotmail.com>
2021-10-22 21:36:06 +0900

    Added react-router-dom and enabled it in index.js and App.js. Also debugged reportWebVitals.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
-import AboutView from './components/AboutView'
+import AboutView from './components/AboutView';
+import {Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div >
+    <div>
       <Navbar />
-      <Home />
-      <AboutView />
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/about" component={AboutView} /> 
+        <AboutView />
+      </Switch>
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,8 @@
 const Home = () => {
     return (
-        <>
+        <div>
             <p>Content in here</p>
-        </>
+        </div>
     )
 }
 
