42ca5b934a074ad2543a8d13907a48c19eedc104
soohoonkim <soohoon.kim@hotmail.com>
2021-10-31 21:26:10 +0900

    Added poster image and overview of the movie in the show detail view

--- src/components/MovieView.js
+++ src/components/MovieView.js
@@ -5,20 +5,44 @@ import { useEffect, useState } from 'react';
 const MovieView = () => {
     const { id } = useParams()
     const [movieDetails, setMovieDetails] = useState({})
+    const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
         fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=9bad846e0f8b5d55c7693ccfccbcdcf5&language=en-US`)
         .then(response => response.json())
         .then(data => {
             setMovieDetails(data)
+            setIsLoading(false)
         })
     }, [id])
 
-    return (
-        <div>
-            <Hero text={movieDetails.original_title} />
-        </div>
-    )
+    function renderMovieDetails () {
+        if(isLoading) {
+            return <Hero text="Loading..." />
+        }
+        if(movieDetails) {
+            //TODO: default image if movie poster image does not exist
+            const posterPath = `https://image.tmdb.org/t/p/w500/${movieDetails.poster_path}`
+            return (
+                <div>
+                    <Hero text={movieDetails.original_title} />
+                    <div className="container my-5">
+                        <div className="row">
+                            <div className="col-md-3">
+                            <img src={posterPath} alt="..." className="img-fluid shadow rounded" />
+                            </div>
+                            <div className="col-md-9">
+                            <h2>{movieDetails.original_title}</h2>
+                            <p className="lead">{movieDetails.overview}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )
+        }
+    }
+
+    return renderMovieDetails()
 }
 
 export default MovieView;
