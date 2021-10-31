46f0842cd05d8e172bbb09a011a2a668254397da
soohoonkim <soohoon.kim@hotmail.com>
2021-10-31 10:50:51 +0900

    Created MovieCard component to show poster and image of the searched movies

--- src/components/SearchView.js
+++ src/components/SearchView.js
@@ -2,18 +2,39 @@ import Hero from "./Hero";
 
 //themoviedb API key = 9bad846e0f8b5d55c7693ccfccbcdcf5
 
+const MovieCard = ({movie}) => {
+    const posterUrl = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+    return (
+        <div className="col-lg-3 col-md-4 my-4">
+            <div className="card">
+                <img src={posterUrl} className="card-img-top" alt={movie.original_title} />
+                <div className="card-body">
+                    <h5 className="card-title">{movie.original_title}</h5>
+                    <a href="#" className="btn btn-primary">Show details</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
 const SearchView = ({ keyword, searchResults }) => {
 
     const title = `You are searching for ${keyword}`
 
     const resultsHtml = searchResults.map((obj, i) => {
-        return <div key={i}>{obj.original_title}</div>
+        return <MovieCard movie={obj} key={i} />
     })
 
     return (
         <div>
             <Hero text={title} />
-            {resultsHtml}
+            {resultsHtml &&
+                <div className="container">
+                    <div className="row">
+                        {resultsHtml}
+                    </div>
+                </div>
+            }
         </div>
     )
 }
