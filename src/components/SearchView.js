bec0910f7338d07aea74367d7b22cbd577604561
soohoonkim <soohoon.kim@hotmail.com>
2021-11-07 21:57:36 +0900

    Created if block to show image not available in searchview as well

--- src/components/SearchView.js
+++ src/components/SearchView.js
@@ -1,11 +1,27 @@
 import Hero from "./Hero";
 import { Link } from 'react-router-dom';
+import missingImage from './icons8-image-file-240.png';
 
 //themoviedb API key = 9bad846e0f8b5d55c7693ccfccbcdcf5
 
 const MovieCard = ({movie}) => {
     const posterUrl = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
     const detailUrl = `/movie/${movie.id}`
+
+    if (movie.poster_path == null) {
+        return (
+            <div className="col-lg-3 col-md-4 my-4">
+                <div className="card">
+                    <img src={missingImage} className="card-img-top" alt={movie.original_title} />
+                    <p className="text-center">Image Not Available</p>
+                    <div className="card-body">
+                        <h5 className="card-title">{movie.original_title}</h5>
+                        <Link to={detailUrl} className="btn btn-primary">Show details</Link>
+                    </div>
+                </div>
+            </div>
+        )
+    }
     return (
         <div className="col-lg-3 col-md-4 my-4">
             <div className="card">
